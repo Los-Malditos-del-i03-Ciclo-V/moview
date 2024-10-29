ca6913ac9cec86edb228a6b0903591e6b2c41900
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-29 18:04:27 -0500

    feat(peliculas): Se implementa la edición de películas mediante una nueva vista
    Agrega la funcionalidad para editar películas por ID mediante una nueva vista y ruta.
    
    - Se crea la vista "editar-pelicula" para mostrar la información de la película a editar mediante la creación de una nueva vista.
    - Se define la ruta "/actualizar/{id}" para acceder a la vista de edición, pasando el ID de la película mediante la creación de una nueva ruta.
    - Se implementa la lógica para obtener la película con el ID especificado y mostrarla en la vista mediante la obtención de la película por ID y su posterior visualización en la vista.

--- controller/src/main/java/net/api/pelicula/MoviesController.java
+++ controller/src/main/java/net/api/pelicula/MoviesController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -26,4 +28,15 @@ public class MoviesController {
         model.addAttribute("listaMovies", listaMovies);
         return "movies/movies";
     }
+
+
+    // === Path for update a movie by id === //
+    @GetMapping("/actualizar/{id}")
+    public ModelAndView editarPelicula (@PathVariable(name = "id") int id){
+        ModelAndView modelAndView = new ModelAndView("/peliculas/editar-pelicula");
+        Pelicula pelicula = service.searchObjectById(id);
+        modelAndView.addObject("pelicula", pelicula);
+        return modelAndView;
+    }
+
 }
