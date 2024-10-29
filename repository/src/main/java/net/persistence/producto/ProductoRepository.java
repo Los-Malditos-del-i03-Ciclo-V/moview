004ea0a5032c7e257b803a98fbb9d1556f5fdb7d
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-29 17:12:49 -0500

    refactor(producto): se comenta el repositorio de productos
    Se comenta la definición del repositorio `ProductoRepository` ya que aún no se implementa.

---
+++ repository/src/main/java/net/persistence/producto/ProductoRepository.java
@@ -0,0 +1,7 @@
+//package net.persistence.producto;
+//
+//import net.entity.producto.Producto;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+//}
