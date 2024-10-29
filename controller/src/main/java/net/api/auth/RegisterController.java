f3de29b911fad6c6a1c7e67405a32473def3334b
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-29 16:46:17 -0500

    refactor(auth): se actualiza la ruta del controlador de registro
    Se actualiza la ruta del controlador de registro de `/register` a `/autenticacion/register` para mejorar la organización y la coherencia del ruteo de la aplicación.

--- controller/src/main/java/net/api/auth/RegisterController.java
+++ controller/src/main/java/net/api/auth/RegisterController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/register")
+@RequestMapping("/autenticacion")
 public class RegisterController {
 
-    @GetMapping
+    @GetMapping("/register")
     public String mostrarRegister() {
         return "register/register";
     }
