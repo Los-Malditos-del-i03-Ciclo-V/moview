5cce8d82f4a19aa0f4a08307f1cb07a6ef4acb9a
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-29 16:52:46 -0500

    feat(peliculas): se agrega el controlador de peliculas para el usuario
    Se agrega el controlador de peliculas para gestionar las rutas relacionadas con las peliculas.
    Se incluye el mapeo de la ruta `/peliculas` para mostrar la lista de películas.
    El controlador utiliza el servicio `PeliculaService` para obtener la lista de películas y se la pasa al modelo.

--- controller/src/main/java/net/api/pelicula/MoviesController.java
+++ controller/src/main/java/net/api/pelicula/MoviesController.java
@@ -1,16 +1,29 @@
 package net.api.pelicula;
 
+import net.business.pelicula.PeliculaService;
+import net.entity.pelicula.Pelicula;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/movies")
+@RequestMapping("/peliculas")
 
 public class MoviesController {
 
+
+    @Autowired
+    private PeliculaService service;
+
     @GetMapping
-    public String mostrarMovies() {
+    public String mostrarMovies(Model model) {
+        List<Pelicula> listaMovies = service.searchAllObjects();
+
+        model.addAttribute("listaMovies", listaMovies);
         return "movies/movies";
     }
 }
