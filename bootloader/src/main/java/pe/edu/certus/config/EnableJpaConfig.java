7edd4bbeabc65de49b4124d8fd51221d680d8d2b
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-11-25 22:16:09 -0500

    feat(config): se actualiza la configuración de la base de datos y dependencias
    Este commit refactoriza la inicialización de la base de datos y actualiza las dependencias, resolviendo problemas con la creación del esquema en reinicios posteriores de la aplicación.
    
    - Se añadió `application.log` para incluir un registro más detallado, proporcionando información valiosa para la depuración de operaciones de la base de datos.
    
    - Se actualizó `EnableJpaConfig.java` (en `bootloader`)
    
    - Se actualizaron los archivos `pom.xml` en los módulos `controller`, `domain` y `service` para asegurar que todas las dependencias necesarias para el nuevo método de inicialización de la base de datos estén presentes y sean compatibles.
    
    Este cambio asegura que el esquema de la base de datos se inicialice y mantenga correctamente en reinicios posteriores de la aplicación sin la necesidad de usar `create-drop`, lo cual es poco fiable y puede conllevar la pérdida de datos.

--- bootloader/src/main/java/pe/edu/certus/config/EnableJpaConfig.java
+++ bootloader/src/main/java/pe/edu/certus/config/EnableJpaConfig.java
@@ -1,7 +1,9 @@
 package pe.edu.certus.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackages = {"pe.edu.certus.persistence"})
 public class EnableJpaConfig {
 }
