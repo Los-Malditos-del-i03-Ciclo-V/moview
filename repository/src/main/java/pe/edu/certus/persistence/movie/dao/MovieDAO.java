b35d30b02f168f48150267762ecfbaa9be530606
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-11-26 20:29:11 -0500

    chore (entities): se define la entidad MovieDAO
    - Se creó la clase `MovieDAO` para representar una película en la base de datos, utilizando anotaciones JPA (`@Entity`, `@Table`, `@Id`, `@Column`, etc.).
    - La clase incluye campos para los detalles de la película (título, director, sinopsis, etc.) y utiliza anotaciones de Lombok (`@Data`, `@Builder`, `@AllArgsConstructor`, `@NoArgsConstructor`) para la generación de código, lo que hace el código más conciso y reduce el código repetitivo.

--- repository/src/main/java/pe/edu/certus/persistence/movie/dao/MovieDAO.java
+++ repository/src/main/java/pe/edu/certus/persistence/movie/dao/MovieDAO.java
@@ -20,15 +20,34 @@ public class MovieDAO {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "director")
     private String director;
+
+    @Column(name = "synopsis")
     private String synopsis;
+
+    @Column(name = "image")
     private String image;
+
+    @Column(name = "duration")
     private String duration;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "clasification")
     private String clasification;
+
+    @Column(name = "language")
     private String language;
+
+    @Column(name = "category")
     private String category;
+
+    @Column(name = "format")
     private String format;
 
 }
