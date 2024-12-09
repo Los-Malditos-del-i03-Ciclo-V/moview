093f41db6f2c21168936e8be336c52e97cdd4300
Piero Revolledo <63229235@certus.edu.pe>
2024-12-09 00:22:00 -0500

    actualizacion

--- controller/src/main/java/pe/edu/certus/api/order/OrdenController.java
+++ controller/src/main/java/pe/edu/certus/api/order/OrdenController.java
@@ -1,47 +1,17 @@
 package pe.edu.certus.api.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import pe.edu.certus.adapter.orden.OrdenMethodsAdapter;
-import pe.edu.certus.model.order.dto.OrderDTO;
-import pe.edu.certus.persistence.order.adapters.OrderConvertersAdapter;
-import pe.edu.certus.persistence.order.dao.OrderDAO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/orders")
+@RequestMapping("/payment")
 public class OrdenController {
-
-    private final String ORDER_PAGE = "orden/orden";
-
-    @Autowired
-    private OrdenMethodsAdapter ordenMethodsAdapter;
-
-    @Autowired
-    private OrderConvertersAdapter orderConvertersAdapter;
+    private static final String ORDEN_PAGE = "payment/pasarela_pago";
 
     @GetMapping
-    public String showOrderPage() {
-        return ORDER_PAGE;
-    }
-
-
-    @PostMapping("/save")
-    public String saveOrderPage(@ModelAttribute ("order") OrderDAO orderDAO, RedirectAttributes redirectAttributes) {
-
-        OrderDTO orderDTO = orderConvertersAdapter.convertToDto(orderDAO);
-        ordenMethodsAdapter.updateEntity(orderDTO);
-        
-        redirectAttributes.addFlashAttribute("message", "Orden guardada con éxito!");
-        return "redirect:/orders";
-
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteOrder(@PathVariable(name = "id") Integer id){
-        ordenMethodsAdapter.deleteEntityById(id);
-        return "redirect:/orders";
+    public String displayOrdenPage(){
+        return ORDEN_PAGE;
     }
 
 }
